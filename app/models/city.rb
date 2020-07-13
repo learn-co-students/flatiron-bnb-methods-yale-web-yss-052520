c93efd1cb55f1303ce885f0fc164d952d055e14a
chad <chad.palmer@yale.edu>
2020-07-13 12:09:47 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,23 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
+
+  def city_openings(startdate, enddate)
+    listings.select {|l| l.available(startdate.to_datetime, enddate.to_datetime) }
+  end
+
+  def self.highest_ratio_res_to_listings
+    all.max_by {|city| city.ratio_res_to_listings}
+  end
+
+  def ratio_res_to_listings
+    listings.count == 0 ? 0 : reservations.count / listings.count
+  end
+
+  def self.most_res
+    all.max_by {|city| city.reservations.count}
+  end
 
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,29 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood, presence: true
+
+  before_create do 
+    self.host.update(host: true)
+  end
+
+  before_destroy do
+    if self.host.listings.count == 1
+      self.host.update(host: false)
+    end
+  end
+
+  def available(startdate, enddate)
+    !reservations.find {|reservation| reservation.checkin.between?(startdate - 1, enddate + 1) || reservation.checkout.between?(startdate - 1, enddate + 1) }
+  end
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
 end
